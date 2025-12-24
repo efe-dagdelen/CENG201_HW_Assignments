469685b82e769c0aab2a50531406ffab7aeeac3b
efe-dagdelen <efe_dagdelen@outlook.com>
2025-12-24 16:50:54 +0300

    task1

---
+++ src/PatientList.java
@@ -0,0 +1,96 @@
+public class PatientList {
+
+    public static class Node {
+        Patient data;
+        Node next;
+
+        Node(Patient patient) {
+            this.data = patient;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public PatientList() {               //constructor
+        this.head = null;
+    }
+
+
+    public void addPatient(Patient p) {
+        Node patient = new Node(p);          // new node(patient)
+        if (head == null) {
+            head = patient;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = patient;
+
+        }
+
+    }
+
+
+    public void removePatient(int id) {
+        if (head == null) {
+            System.out.println("no patient to delete was found");
+            return;
+        }
+
+        if (head.data.getId() == id) {
+            head = head.next;
+            return;
+        }
+
+        Node prev = head;
+        Node current = head;
+
+        while (current != null) {
+            if (current.data.getId() == id) {
+                prev.next = current.next;
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+
+    }
+
+
+    public Patient findPatient(int id) {
+        Node current = head;
+
+
+        while (current != null) {
+            if (current.data.getId() == id) {
+                System.out.println("found patient with id " + id);    // to give information
+                return current.data;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("no patient to display");
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.println("Patient id: " + current.data.getId() + " , "
+            + " Name: " + current.data.getName());
+
+            current = current.next;
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ src/PatientListTest.java
@@ -0,0 +1,26 @@
+public class PatientListTest {
+
+public static void main(String[] args) {
+
+    PatientList patientList = new PatientList();
+
+    patientList.addPatient(new Patient(120, "Zeki Güldür", 5, 30));
+    patientList.addPatient(new Patient(121,"Çağatay Han",2,25));
+    patientList.addPatient(new Patient(122,"Ahmet Kaygılı",4,55));
+    patientList.addPatient(new Patient(123,"Orhan Elmas",3,43));
+    patientList.addPatient(new Patient(124,"Arda Cengiz",1,18));
+
+    patientList.removePatient(122);
+
+    patientList.findPatient(124);
+
+    patientList.printList();
+
+}
+
+
+
+
+
+
+}
